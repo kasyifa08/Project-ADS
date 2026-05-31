df08e233d57fcfdfdf1abde0eb75d333c6fe9125
KasyifaNaila <kasyifa05@gmail.com>
2026-06-01 01:35:20 +0700

    Fix cloudinary

--- app/cloudinary_config.py
+++ app/cloudinary_config.py
@@ -1,11 +1,4 @@
-import os
 import cloudinary
-from dotenv import load_dotenv
+import os
 
-load_dotenv()
-
-cloudinary.config(
-    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
-    api_key=os.getenv("CLOUDINARY_API_KEY"),
-    api_secret=os.getenv("CLOUDINARY_API_SECRET")
-)
+cloudinary.config(cloudinary_url=os.getenv("CLOUDINARY_URL"))
