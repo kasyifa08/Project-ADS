fb0066595fc5d82ff9f1dd1f1a467be710b42839
KasyifaNaila <kasyifa05@gmail.com>
2026-06-01 01:06:23 +0700

    Fix cloudinary

--- app/cloudinary_config.py
+++ app/cloudinary_config.py
@@ -9,7 +9,3 @@ cloudinary.config(
     api_key=os.getenv("CLOUDINARY_API_KEY"),
     api_secret=os.getenv("CLOUDINARY_API_SECRET")
 )
-
-print("CLOUD NAME:", os.getenv("CLOUDINARY_CLOUD_NAME"))
-print("API KEY:", os.getenv("CLOUDINARY_API_KEY"))
-print("API SECRET:", os.getenv("CLOUDINARY_API_SECRET"))
