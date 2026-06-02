2235dd550d5507899017704f10a348ddd9bd062b
KasyifaNaila <kasyifa05@gmail.com>
2026-06-02 09:08:53 +0700

    Fix cloudinary

--- Backend/app/cloudinary_config.py
+++ Backend/app/cloudinary_config.py
@@ -1,4 +1,4 @@
 import cloudinary
 import os
 
-cloudinary.config(cloudinary_url=os.getenv("CLOUDINARY_URL"))
+cloudinary.config(cloudinary_url=os.getenv("cloudinary://224784319473746:abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@df5ev7gh3"))
